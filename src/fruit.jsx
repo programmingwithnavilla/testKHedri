6b3d566e2e80837402d8065b3ccfb373c52ec9c8
navidbarsalari2016 <navidbarsalari2016@gmail.com>
2021-08-01 09:26:34 +0430

    delet all

--- src/fruit.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import g from "react-dom";
-// const { render } = require("react-dom");
-
-export function Fruit(type) {
-  console.log("functional fruit", type);
-  return <h3>hey {type}</h3>;
-}
